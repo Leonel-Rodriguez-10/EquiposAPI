e1ba5a428aeadb81e7b5ce772c6c35424e6eeaff
Leonel Rodríguez <leonelro2006@gmail.com>
2025-11-05 01:06:17 -0600

    Add files via upload

---
+++ EquiposAPI/src/main/java/com/intecap/EquiposAPI/EquiposAPIApplication.java
@@ -0,0 +1,11 @@
+package com.intecap.EquiposAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquiposAPIApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EquiposAPIApplication.class, args);
+  }
+}
