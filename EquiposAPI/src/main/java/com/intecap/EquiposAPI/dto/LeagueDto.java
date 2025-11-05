56557783188df8b440fd4e5bb4a7032d1720e073
Leonel Rodríguez <leonelro2006@gmail.com>
2025-11-05 00:06:27 -0600

    Delete EquiposAPI directory

--- EquiposAPI/src/main/java/com/intecap/EquiposAPI/dto/LeagueDto.java
+++
@@ -1,2 +0,0 @@
-package com.intecap.EquiposAPI.dto;
-public record LeagueDto(Integer id, String name, String country) {}
--- EquiposAPI/src/main/java/com/intecap/EquiposAPI/dto/MatchDto.java
+++
@@ -1,2 +0,0 @@
-package com.intecap.EquiposAPI.dto;
-public record MatchDto(Long id, String utcKickoff, String status, String home, String away, Integer homeScore, Integer awayScore) {}
